1a2d2f25d64cb35573f4e4c6a3b6cf2685ab6ae3
Carlos <carloseduardoperetiatko07@gmail.com>
2026-03-05 00:19:00 -0300

    Delete L01-alg directory

--- L01-alg/exe2.py
+++
@@ -1,7 +0,0 @@
-# 2. Saudação. Escreva um programa que pergunte o nome do(a) usuário(a) e em seguida exiba
-# uma mensagem de “Olá" ao usuário, utilizando o nome fornecido.
-
-nome = input("Qual e seu nome ? ")
-
-print("Olá, " + nome)
-
--- L01-alg/exe4.py
+++
@@ -1,6 +0,0 @@
-# Área de um terreno. Crie um programa Python que leia as dimensões de um terreno em
-# metros (largura e profundidade), e exiba o resultado em hectares.
-
-larg = float(input("Insira a largura do seu terreno: "))
-prof = float(input("Insira a profundidade do seu terreno: "))
-
